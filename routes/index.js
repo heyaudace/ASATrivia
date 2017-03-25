1db879b2e470906cbd78309130491aa07f91f318
Robert Vunabandi <rvunabandi@gmail.com>
2017-03-25 17:04:20 -0400

    added stuffs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page for players of the game. They should be prompted to input code or link. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'ASA TRIVIA' });
+});
+
+/* GET moderator page. Moderator can start a new game. */
+router.get('/create', function(req, res, next) {
+  res.render('moderator', { title: 'ASA TRIVIA | Create New Game' });
+});
+
+router.get('/newgame', function(req, res, next) {
+		var question = req.questions_per_round*req.rounds;
+	// res.render('moderator', { title: 'ASA TRIVIA | Create New Game' });
 });
 
 module.exports = router;
